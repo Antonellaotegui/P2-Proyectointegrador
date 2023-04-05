10b16558b51079cf67dffc56635b80f0de7966bd
Antonellaotegui <aotegui@udesa.edu.ar>
2023-04-05 15:37:48 -0300

    algunas rutas y mejoras en db

--- P2-Proyectointegrador/db/index.js
+++ P2-Proyectointegrador/db/index.js
@@ -1,80 +1,81 @@
-const controlador = require("../controllers/bandasControllers")
-usuario={
-  objetoliteral: {
-    Usuario: "Antonella Otegui",
-    DNI: "45678912",
-    Email: "antonellaotegui@icloud.com",
-    Contraseña: "12345678",
-    Fecha:"22/02/2004",
-    Fotodeperfil:"",
-
-}}
-
-productos = {
-    lista: [
-      {
-          imagen: "https://images.app.goo.gl/Y8xrePBQfTEtNw5fA",
-          nombre: "Palo de Hockey Grays",
-          descripcion: "El Grays Blast Ultrabow Pink / Teal es el palo de inicio perfecto para los niños y cualquiera que esté comenzando su carrera en el hockey. El palo de hockey Blast tiene un mango de gamuza de color que hace que usar el palo sea una experiencia cómoda.",
-          fecha_de_carga: "16/7/2022",
-      },{
-        imagen: "https://images.app.goo.gl/yH5Rpq6mcFnxjfbc8",
-        nombre: "Canilleras Malik",
-        descripcion: "Conocé las Canilleras Malik Club 8 Protección, ideales para cuidarte en cada enfrentamiento con el oponente. Diseñadas en una estructura curva y anatómica que se adapta a las piernas perfecta y cómodamente, tienen un ajuste de correas para facilitar el poner y sacar",
-        fecha_de_carga: "22/2/2016",
-      },{
-        imagen: "https://images.app.goo.gl/B9E2RpWJVfiQuAUh6",
-        nombre: "Protector bucal Gilbert",
-        descripcion: "Minimizá el riesgo de lesiones con el Protector bucal Gilbert Viper Pro 3. Su simplicidad en el diseño permite una perfecta adaptación a cada tipo de dentadura, impidiendo los golpes y reduciendo las vibraciones para mayor cuidado en el campo de juego.",
-        fecha_de_carga: "24/4/2020",
-      },{
-        imagen: "https://images.app.goo.gl/qYcZKeepZAjUi5Vf6",
-        nombre: "Funda TK",
-        descripcion: "La imbatible TK 3.3 ofrece una solución efectiva en todos los entornos, con un compartimiento para palo, 2 compartimientos frontales separados, y una única tira trasera.",
-        fecha_de_carga: "3/6/2021",
-      },{
-        imagen: "https://images.app.goo.gl/EE2vCZ6ZkinK9TD48",
-        nombre: "Cubre canilleras",
-        descripcion: "Protegen la piel del roce generado por la canillera durante el entrenamineto y partido. Ofrecen una buena elasticidad y una leve compresion.",
-        fecha_de_carga: "17/1/2023",
-      },{
-        imagen: "https://images.app.goo.gl/uCSEi63t5CoMJYPt7",
-        nombre: "Tartaneras Adidas",
-        descripcion: "Es un botín clásico, tradicional en su perfil de cosmética, ampliamente testeado en calidad y duración. Está realizado en materiales sintéticos sin costuras y con detalles de alta frecuencia, lo que hace liviano y cómodo.",
-        fecha_de_carga: "5/5/2019",
-      },{
-        imagen: "https://images.app.goo.gl/1rnW7TmppctqLWWEA",
-        nombre: "Bocha de hockey Drial",
-        descripcion: "Bochas profesionales, estructura confeccionada en una sola pieza hueca, 0% absorción de agua y humedad.",
-        fecha_de_carga: "",
-      },{
-        imagen: "https://images.app.goo.gl/QEfVpPdxynEC1jb29",
-        nombre: "Guante Vlack",
-        descripcion: "De la línea de guantes Brabo, el F5 es un guante confeccionado en tela y con protecciones de plástico moldeado. Es abierto, lo cual brinda mucha comodidad al jugador para que pueda seguir sintiendo el mango del palo con la palma de la mano.",
-        fecha_de_carga: "12/2/2022",
-      },{
-        imagen: "https://images.app.goo.gl/s6x1TT8C8N5ry8kn9",
-        nombre: "Guante Drial de arqueros",
-        descripcion: "Fabricado con goma spum de alta densidad, con terminaciones de calidad. Todos nuestros productos poseen 90 días de garantía por defectos de fabricación, desde la fecha de compra.",
-        fecha_de_carga: "13/8/2018",
-      },{
-        imagen: "https://images.app.goo.gl/99EyfHL51FrcmZ7x5",
-        nombre: "Palo Balling",
-        descripcion: "El nuevo Alpha 100 Xtreme Groove Lowbow fue diseñado para arrastradores en busca de sumar potencia a sus tiros y progresar en sus habilidades aereas. ",
-        fecha_de_carga: "31/12/2017",
-      }
+objliteral={
+  usuario={
+      Usuario: "Antonella Otegui",
+      DNI: "45678912",
+      Email: "antonellaotegui@icloud.com",
+      Contraseña: "12345678",
+      Fecha:"22/02/2004",
+      Fotodeperfil:"images/users/default-images.png",
+  
+  },
+  
+  productos = {
+      lista: [
+        {
+            imagen: "https://images.app.goo.gl/Y8xrePBQfTEtNw5fA",
+            nombre: "Palo de Hockey Grays",
+            descripcion: "El Grays Blast Ultrabow Pink / Teal es el palo de inicio perfecto para los niños y cualquiera que esté comenzando su carrera en el hockey. El palo de hockey Blast tiene un mango de gamuza de color que hace que usar el palo sea una experiencia cómoda.",
+            fecha_de_carga: "16/7/2022",
+        },{
+          imagen: "https://images.app.goo.gl/yH5Rpq6mcFnxjfbc8",
+          nombre: "Canilleras Malik",
+          descripcion: "Conocé las Canilleras Malik Club 8 Protección, ideales para cuidarte en cada enfrentamiento con el oponente. Diseñadas en una estructura curva y anatómica que se adapta a las piernas perfecta y cómodamente, tienen un ajuste de correas para facilitar el poner y sacar",
+          fecha_de_carga: "22/2/2016",
+        },{
+          imagen: "https://images.app.goo.gl/B9E2RpWJVfiQuAUh6",
+          nombre: "Protector bucal Gilbert",
+          descripcion: "Minimizá el riesgo de lesiones con el Protector bucal Gilbert Viper Pro 3. Su simplicidad en el diseño permite una perfecta adaptación a cada tipo de dentadura, impidiendo los golpes y reduciendo las vibraciones para mayor cuidado en el campo de juego.",
+          fecha_de_carga: "24/4/2020",
+        },{
+          imagen: "https://images.app.goo.gl/qYcZKeepZAjUi5Vf6",
+          nombre: "Funda TK",
+          descripcion: "La imbatible TK 3.3 ofrece una solución efectiva en todos los entornos, con un compartimiento para palo, 2 compartimientos frontales separados, y una única tira trasera.",
+          fecha_de_carga: "3/6/2021",
+        },{
+          imagen: "https://images.app.goo.gl/EE2vCZ6ZkinK9TD48",
+          nombre: "Cubre canilleras",
+          descripcion: "Protegen la piel del roce generado por la canillera durante el entrenamineto y partido. Ofrecen una buena elasticidad y una leve compresion.",
+          fecha_de_carga: "17/1/2023",
+        },{
+          imagen: "https://images.app.goo.gl/uCSEi63t5CoMJYPt7",
+          nombre: "Tartaneras Adidas",
+          descripcion: "Es un botín clásico, tradicional en su perfil de cosmética, ampliamente testeado en calidad y duración. Está realizado en materiales sintéticos sin costuras y con detalles de alta frecuencia, lo que hace liviano y cómodo.",
+          fecha_de_carga: "5/5/2019",
+        },{
+          imagen: "https://images.app.goo.gl/1rnW7TmppctqLWWEA",
+          nombre: "Bocha de hockey Drial",
+          descripcion: "Bochas profesionales, estructura confeccionada en una sola pieza hueca, 0% absorción de agua y humedad.",
+          fecha_de_carga: "",
+        },{
+          imagen: "https://images.app.goo.gl/QEfVpPdxynEC1jb29",
+          nombre: "Guante Vlack",
+          descripcion: "De la línea de guantes Brabo, el F5 es un guante confeccionado en tela y con protecciones de plástico moldeado. Es abierto, lo cual brinda mucha comodidad al jugador para que pueda seguir sintiendo el mango del palo con la palma de la mano.",
+          fecha_de_carga: "12/2/2022",
+        },{
+          imagen: "https://images.app.goo.gl/s6x1TT8C8N5ry8kn9",
+          nombre: "Guante Drial de arqueros",
+          descripcion: "Fabricado con goma spum de alta densidad, con terminaciones de calidad. Todos nuestros productos poseen 90 días de garantía por defectos de fabricación, desde la fecha de compra.",
+          fecha_de_carga: "13/8/2018",
+        },{
+          imagen: "https://images.app.goo.gl/99EyfHL51FrcmZ7x5",
+          nombre: "Palo Balling",
+          descripcion: "El nuevo Alpha 100 Xtreme Groove Lowbow fue diseñado para arrastradores en busca de sumar potencia a sus tiros y progresar en sus habilidades aereas. ",
+          fecha_de_carga: "31/12/2017",
+        }
+      ]
+    },
+  
+    comentarios=[
+        {
+          usuario:"Antonella Otegui",
+          comentario:"comentario",
+          imagendeperfil:"images/users/default-images.png"
+        }
     ]
-  }
-
-  comentarios={
-    listac:[
-      {
-        usuario:"Antonella Otegui",
-        comentario:"comentario",
-        imagendeperfil:"poner imagen de maqueta"
-      }
-    ]
-
-  }
-
-  module.exports = bandas
+      
+      
+  
+    }
+  
+    module.exports = bandas
+}
--- P2-Proyectointegrador/routes/products.js
+++ P2-Proyectointegrador/routes/products.js
@@ -2,4 +2,5 @@ const express = require ("express")
 const productscontroller = require("../controllers/productscontroller")
 const router= express.Router()
 
+router.get("/products/productadd", userscontrollers.productadd)
 
