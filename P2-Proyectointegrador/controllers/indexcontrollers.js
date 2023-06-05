800b660773c9dbcc57b47e7d402b5189814f9a75
Antonellaotegui <aotegui@udesa.edu.ar>
2023-06-05 15:44:16 -0300

    mis cambios en los controllers

--- P2-Proyectointegrador/controllers/indexcontrollers.js
+++ P2-Proyectointegrador/controllers/indexcontrollers.js
@@ -1,15 +1,30 @@
-const objliteral= require("../db/index") 
+const objliteral= require("../db/index")
 let usuarioslista= objliteral.usuario
 let productoslista= objliteral.productos
 let comentarioslista= objliteral.comentarios
+const db = require("../database/models/index")
 const index={
     index: function (req, res){
-    return res.render('index',{
-        usuarioslista: usuarioslista,
-        productoslista: productoslista,
-        comentarioslista: comentarioslista,
-        userlogueado:false
-    })
+        db.Movies.findAll({
+            raw: true,
+            nest:true,
+            include:[
+                {association: 'generos'},
+                {association: 'actores'}
+            ]
+            })
+        .then(function(data){
+            console.log(data)
+            return res.render('index',{
+                usuarioslista: usuarioslista,
+                data : data,
+                productoslista: productoslista,
+                comentarioslista: comentarioslista,
+                userlogueado:false
+            })
+        })
+        .catch(function(err){console.log(err)})
+    
 }
 }
 
