dbe5e50e4f0347cf5f579324ad055ac6a5d8a2ce
clarapelllet <cpelletlastra@udesa.edu.ar>
2023-06-05 16:01:10 -0300

    ahora no nos va a romper

--- P2-Proyectointegrador/app.js
+++ P2-Proyectointegrador/app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('.//index');
-var usersRouter = require('./routes/users');
-const productsRouter= require('./routes/products');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
