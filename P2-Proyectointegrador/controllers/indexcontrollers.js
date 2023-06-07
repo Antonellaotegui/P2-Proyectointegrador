9ebf6c51c7495864355559bf088e04fca4dc2df9
pilirolon <prolon@udesa.edu.ar>
2023-06-07 16:52:40 -0300

    algo de controllers

--- P2-Proyectointegrador/controllers/indexcontrollers.js
+++ P2-Proyectointegrador/controllers/indexcontrollers.js
@@ -3,10 +3,17 @@ let usuarioslista= objliteral.usuario
 let productoslista= objliteral.productos
 let comentarioslista= objliteral.comentarios
 const db = require("../database/models/index")
+const op = db.Sequelize.Op
+
 const index={
     index: function (req, res){
-        db.Movies.findAll({
-            raw: true,
+        db.productos.findAll({  //estoy accediendo al modelo prdocuto.js el find all esta recibiendo una info en forma de proemsa y la esta logrando operar atrves de un console log
+           where:{
+            id:{
+                [op.gt] : 12 // productos mayor que 12, NOSE SI VAMOS A NECESITARLO 
+            }
+           },
+            raw: true, //array limpio sin cholco de info, datos que no me sirven y no necestio. Para manejar la ifno de una manera limpia y optima
             nest:true,
             include:[
                 {association: 'generos'},
@@ -15,7 +22,7 @@ const index={
             })
         .then(function(data){
             console.log(data)
-            return res.render('index',{
+            res.render('index',{
                 usuarioslista: usuarioslista,
                 data : data,
                 productoslista: productoslista,
@@ -25,7 +32,14 @@ const index={
         })
         .catch(function(err){console.log(err)})
     
-}
+        db.Comentarios.findAll({raw:true})
+        .then(function(data){
+          console.log(data)  
+        })
+        .catch(function(err){ //esrr se puede llamar de cualq manera
+            console.log(err)
+        })
+        } //data se puede llamar de cualq manera
 }
 
 module.exports = index;
